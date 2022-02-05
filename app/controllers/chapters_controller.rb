eb38088caa4a28418189e3583ac75bb0065304cd
Dean DeHart <DeHartDean@gmail.com>
2022-02-04 22:55:53 -0500

    Refactor to one create method

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -15,22 +15,10 @@ class ChaptersController < ApplicationController
 
   # POST /chapters
   def create
-    @chapter = Chapter.new(chapter_params)
+    @chapter = find_or_create_book_for_chapter
 
     if @chapter.save
       render json: @chapter, status: :created, location: @chapter
-    else
-      render json: @chapter.errors, status: :unprocessable_entity
-    end
-  end
-
-  def create_and_return_new_book
-    @book = Book.create(author: chapter_params[:author])
-    @chapter = Chapter.new(chapter_params)
-    @chapter.book = @book
-
-    if @chapter.save
-      render json: @book.to_json(include: :chapters), status: :created, location: @chapter
     else
       render json: @chapter.errors, status: :unprocessable_entity
     end
@@ -51,6 +39,30 @@ class ChaptersController < ApplicationController
   end
 
   private
+
+  # Creates a book if one does not already exist for this chapter
+  # then returns the chapter.
+  def find_or_create_book_for_chapter
+    chapter = Chapter.new(chapter_params)
+
+    id_was_not_provided = params[:chapter][:book_id].nil?
+    if id_was_not_provided
+      create_book_for_chapter(chapter)
+    else
+      chapter.book = Book.find_or_create_by(id: params[:chapter][:book_id]) do |book|
+        book.author = chapter_params[:author]
+      end
+    end
+    chapter
+  end
+
+  # Create a new book for the chapter.
+  def create_book_for_chapter
+    book = Book.create(author: chapter_params[:author])
+    chapter = Chapter.new(chapter_params)
+    chapter.book = book
+    chapter
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_chapter
